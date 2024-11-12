c93b9882af9a313dce91e7437c732b336850ad3b
vemula <vemulabalaji1998@gmail.com>
2024-11-12 18:03:25 +0530

    Added timer start and stop code

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+
+  // We can use useState for timer but we used useRef because
+  // we don't have any requirement to re-evaluate or reload the component whenever timer changes 
+  // and it doesn't have dependency in UI/JSX code
+  const timer = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>'You lost!'</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start/Stop Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p>Time is running .../Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running" : "Timer inactive"}
+      </p>
     </section>
   );
 }
