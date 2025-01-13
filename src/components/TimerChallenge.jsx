182dc0f7c5ad26d2ab158f9bc31828e27d9aa4f3
vemula <vemulabalaji1998@gmail.com>
2025-01-13 16:02:09 +0530

    using setInterval instead of setTimeOut

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,43 +2,57 @@ import React, { useState, useRef } from "react";
 import RequestModal from "./RequestModal";
 
 export default function TimerChallenge({ title, targetTime }) {
-
   // We can use useState for timer but we used useRef because
-  // we don't have any requirement to re-evaluate or reload the component whenever timer changes 
+  // we don't have any requirement to re-evaluate or reload the component whenever timer changes
   // and it doesn't have dependency in UI/JSX code
   const timer = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-  function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, targetTime * 1000);
-    setTimerStarted(true);
+  const isTimerActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  // This is to trigger the modal dialog when the timer expires
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
   }
 
+  // We changed the setTimeout to setInterval because
+  // 1. SetTimeout runs the function only once ofter the timer set 10 milliseconds
+  //    (previously it ran only once for targetTime * 1000 for setTimeout and now it will run 
+  //    only once after 10 milliseconds )
+  // 2. We need to count the time like how much time is passed and how much time is remaining that
+  //    is why we used setInterval, setInterval runs every time interval here it will run every 
+  //    10 milliseconds that is why we are decrementing the time remaining by 10 milliseconds.
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((preTimeRemaining) => preTimeRemaining - 10);
+    }, 10); // runs every 10 milliseconds
+  }
+
+  // This is to trigger the modal dialog when the user clicks on stop challenge button
   function handleStop() {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialog.current.open();
   }
 
   return (
     <>
-      <RequestModal ref={dialog} targetTime={targetTime} result="lost"/>
+      <RequestModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={isTimerActive ? handleStop : handleStart}>
+            {isTimerActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running" : "Timer inactive"}
+        <p className={isTimerActive ? "active" : undefined}>
+          {isTimerActive ? "Time is running" : "Timer inactive"}
         </p>
       </section>
     </>
