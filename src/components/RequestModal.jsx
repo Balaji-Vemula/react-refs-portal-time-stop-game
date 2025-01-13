7d22c473bea08fa3e7d312e210f948ef8bbb44e8
vemula <vemulabalaji1998@gmail.com>
2025-01-13 17:32:05 +0530

    Code fix

--- src/components/RequestModal.jsx
+++ src/components/RequestModal.jsx
@@ -20,7 +20,7 @@ const RequestModal = forwardRef(function RequestModal({ targetTime, remainingTim
   })
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
       <p>
@@ -29,7 +29,13 @@ const RequestModal = forwardRef(function RequestModal({ targetTime, remainingTim
       <p>
         You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form action="dialog" onSubmit={onReset}>
+      {/* Removed onSubmit to reset because 
+          1. the time is resetting and displaying for a millisecond on screen 
+          2. when user clicks on escape button to dismiss the dialog modal the onSubmit to reset
+            won't be triggered
+          That is why we added onClose to reset to dialog tag and it helps in resetting when 
+          escape button is clicked and won't display the resetted value in UI*/}
+      <form action="dialog"> 
         <button>Close</button>
       </form>
     </dialog>
