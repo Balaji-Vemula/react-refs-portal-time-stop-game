2cf7498f092ff052b3dc820e310cc69743de4198
vemula <vemulabalaji1998@gmail.com>
2025-01-13 16:03:34 +0530

    Small html fixx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={15} />
-        <TimerChallenge title="Not easy" targetTime={10} />
-        <TimerChallenge title="Getting tough" targetTime={5} />
-        <TimerChallenge title="Pros Only" targetTime={1} />
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros Only" targetTime={15} />
       </div>
     </>
   );
